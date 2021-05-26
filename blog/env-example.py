621cf0b01232311f5272d90c31645b13e9cef41a
Ajay Pokharel <ajaypokharel8@gmail.com>
2021-05-26 11:09:26 +0545

    [add] adds env-example file

--- blog/env-example.py
+++ blog/env-example.py
@@ -18,20 +18,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 print(BASE_DIR)
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'm0!y53j_-=fsf$0*4yxbhuf36-vk*j-yl(ze(+*zmlkui%&r+^'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
-
-
-# Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
