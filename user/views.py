461eea0e03ffbd6e4f21af584345a0d6d57389d8
Ajay Pokharel <ajaypokharel8@gmail.com>
2021-06-02 23:51:05 +0545

    [add] adds bookmarks api

--- user/views.py
+++ user/views.py
@@ -1,6 +1,12 @@
 from django.contrib.auth import get_user_model
+from rest_framework.decorators import action
+from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+
+from BlogPost.models import Bookmark
+from BlogPost.serializer import BookmarkSerializer
 from user.permissions import IsUser
 from user.serializer import UserSerializer
 
@@ -8,11 +14,15 @@ USER = get_user_model()
 
 
 class UserViewSet(ModelViewSet):
-    serializer_class = UserSerializer
+    lookup_field = 'username'
+    lookup_url_kwarg = 'username'
+    parser_classes = [MultiPartParser, FormParser, JSONParser]
 
     def get_queryset(self):
         if self.action in ['destroy', 'update', 'partial_update']:
             return USER.objects.all(username=self.request.user.username)
+        if self.action in ['bookmarks']:
+            return Bookmark.objects.filter(user=self.request.user)
         return USER.objects.all()
 
     def get_permissions(self):
@@ -22,3 +32,27 @@ class UserViewSet(ModelViewSet):
         if self.action in ['destroy', 'update', 'partial_update']:
             permission_classes = [IsUser]
         return [permission() for permission in permission_classes]
+
+    def get_serializer_class(self):
+        if self.action in ['bookmarks']:
+            return BookmarkSerializer
+        return UserSerializer
+
+    def get_serializer(self, *args, **kwargs):
+        serializer_class = self.get_serializer_class()
+        kwargs['context'] = self.get_serializer_context()
+        if self.action in ['update', 'partial_update']:
+            kwargs['fields'] = ['email', 'first_name', 'last_name', 'gender']
+        return serializer_class(*args, **kwargs)
+
+    @action(detail=False, methods=['get'])
+    def bookmarks(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
