4edb560faa3e06ec49405747d247e8aef9026f90
Ajay Pokharel <ajaypokharel8@gmail.com>
2021-06-05 23:56:11 +0545

    [add] adds api views for blog comments

---
+++ BlogPost/views/blog_comment.py
@@ -0,0 +1,17 @@
+from rest_framework.permissions import IsAuthenticated
+
+from BlogPost.models import BlogComment
+from BlogPost.serializers.blog_comment import BlogCommentSerializer
+from commons.mixins import RetrieveUpdateDestroyModelMixin
+
+
+class BlogCommentViewSet(RetrieveUpdateDestroyModelMixin):
+    lookup_field = 'uuid'
+    lookup_url_kwarg = 'uuid'
+    serializer_class = BlogCommentSerializer
+
+    def get_queryset(self):
+        return BlogComment.objects.filter(user=self.request.user)
+
+    def get_permissions(self):
+        return [IsAuthenticated]
